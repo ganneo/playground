06350574086a78fecaebb7b345c81839a9eefc68
Neo Gan <neo.gan2015@gmail.com>
2020-07-11 13:16:19 -0700

    refactor the javascript

--- number-game/number.js
+++ number-game/number.js
@@ -17,61 +17,66 @@ function randomNum() {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function playAgain() {
-  document.location.reload();
-}
-
 function message(message, color) {
   input.style.borderColor = color;
   msg.style.display = "block";
   msg.textContent = message;
   msg.style.color = color;
+}
+
+function reload() {
+  window.location.reload();
+}
+
+function restartGame() {
   input.disabled = true;
+  submitBtn.className = "play-again";
+  submitBtn.textContent = "PLAY AGAIN";
+  submitBtn.removeEventListener("click", winOrLose);
+  submitBtn.addEventListener("click", reload);
 }
 
 function winOrLose() {
+  if (
+    isNaN(parseInt(input.value)) ||
+    parseInt(input.value) < min ||
+    parseInt(input.value) > max
+  ) {
+    message("Please guess a number between 1 and 10", "red");
+    setTimeout(function () {
+      input.style.borderColor = "";
+      submitBtn.style.borderColor = "";
+      msg.style.display = "none";
+      input.disabled = false;
+    }, 3000);
+
+    return;
+  }
+
   if (parseInt(input.value) === winningNum) {
+    restartGame();
     message(`${input.value} was correct. YOU WON! ᕕ( ᐛ )ᕗ`, "green");
-    submitBtn.className = "play-again";
-    submitBtn.textContent = "PLAY AGAIN";
-    submitBtn.addEventListener("mousedown", playAgain);
+    return;
+  }
+
+  guessesLeft -= 1;
+
+  if (guessesLeft === 0) {
+    restartGame();
+    message(
+      `${input.value} was incorrect. The number was ${winningNum} :(`,
+      "red"
+    );
+  } else if (guessesLeft === 1) {
+    message(
+      `${input.value} was incorrect. You have ${guessesLeft} guess left`,
+      "red"
+    );
   } else {
-    if (
-      isNaN(parseInt(input.value)) ||
-      parseInt(input.value) < min ||
-      parseInt(input.value) > max
-    ) {
-      message("Please guess a number between 1 and 10", "red");
-      setTimeout(function () {
-        input.style.borderColor = "";
-        submitBtn.style.borderColor = "";
-        msg.style.display = "none";
-        input.disabled = false;
-      }, 3000);
-    } else {
-      guessesLeft -= 1;
-      if (guessesLeft === 0) {
-        submitBtn.className = "play-again";
-        submitBtn.textContent = "PLAY AGAIN";
-        submitBtn.addEventListener("mousedown", playAgain);
-        message(
-          `${input.value} was incorrect. The number was ${winningNum} :(`,
-          "red"
-        );
-      } else {
-        input.style.borderColor = "red";
-        if (guessesLeft === 1) {
-          msg.style.display = "block";
-          msg.textContent = `${input.value} was incorrect. You have ${guessesLeft} guess left`;
-          input.style.disabled = false;
-        } else {
-          msg.style.display = "block";
-          msg.textContent = `${input.value} was incorrect. You have ${guessesLeft} guesses left`;
-          input.style.disabled = false;
-        }
-        msg.style.color = "red";
-      }
-    }
+    message(
+      `${input.value} was incorrect. You have ${guessesLeft} guesses left`,
+      "red"
+    );
   }
 }
 
