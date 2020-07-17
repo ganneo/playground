e5e3d42442d74dc6435ef7eec53fa2b7483ded47
Neo Gan <neo.gan2015@gmail.com>
2020-07-17 13:48:33 -0700

    Add Ajax Joke App

---
+++ joke-maker/joke.js
@@ -0,0 +1,38 @@
+function makeJokes(e) {
+  const number = document.querySelector("input").value;
+  if (!number) {
+    return;
+  }
+
+  const ulElement = document.getElementById("jokes");
+  const ulArray = ulElement.childNodes;
+  ulArray.forEach((ele) => {
+    ele.remove();
+  });
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+  xhr.onload = () => {
+    if (xhr.status !== 200) {
+      console.log(xhr.status);
+      return;
+    }
+
+    const responseObj = JSON.parse(xhr.responseText);
+    const jokeArray = responseObj.value;
+    jokeArray.forEach((ele) => {
+      const joke = ele.joke;
+      const liEle = document.createElement("li");
+      liEle.textContent = joke;
+      ulElement.appendChild(liEle);
+    });
+    document.querySelector("input").value = "";
+  };
+  xhr.onerror = () => {
+    console.log(xhr.responseText);
+  };
+  xhr.send();
+
+  e.preventDefault();
+}
+document.querySelector("button").addEventListener("click", makeJokes);
